df9f88ff8a3df39285ff4d3de04d548044c1fb8f
Mstapleton22 <mike.stapleton2@gmail.com>
2019-01-18 14:04:19 -0700

    deploying heroku

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.use(express.static('public'))
 
-app.get("/stevenuniverse", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.status(200).send({
     stevenuniverse: stevenuniverse
   })
